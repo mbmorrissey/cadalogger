152bbecf59c23c11a2b259ce3e0582f9b9eb10d3
mbmorrissey <michael.morrissey@st-andrews.ac.uk>
2023-05-28 19:44:49 +0100

    add rtc temperature and watchdog functions

--- cadalogger.h
+++ cadalogger.h
@@ -52,7 +52,6 @@ class cadalogger{
     void enable_watchdog();
     void disable_watchdog();
     void feed_watchdog();
-    void feed_watchdog();
     void print_time();
     void write_time_to_rtc();
     double rtc_temp();
